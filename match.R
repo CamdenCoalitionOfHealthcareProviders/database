58a20e47b31f737b4a41c2922548481cf1f08b6f
Jay Fisher <fisher.jc@gmail.com>
2016-03-01 17:14:04 -0500

    update matchr function
    keeps rows on table where foreign key column matches the primary key
    column in another table

--- match.R
+++ match.R
@@ -1,36 +1,55 @@
-# Data Integrity Test 
-# Match each TrackVia table by foreign key
+# Data Integrity Test
+# Keeps rows on table where foreign key column matches the primary key column in another table
+
+# To use matchr function:
+  # Select the two data frames to compare
+    # df1 is the data frame with the PRIMARY KEY
+    # df2 is the data frame with the FOREIGN KEY
+    # Data frames must already be in your global environment in R
+  # Also call the column to you're comparing. The column name MUST be in quotes.
+  # df2_match 
+    # The data frame of df2 where the column you're comparing matches
+    # df2_match drops the rows where the foreign key in df2 does not match the primary key in df1
+    # df2_no_match are the unmatched rows in df2
+  # For now, manually export df2_match
+  # Examples 
+    # matchr(cap_list, pra, 'subscriber_id')
+    # matchr(pra, client_track, 'pra_id')
 
 # Constraints by Table
-# cap_list -- subscriber_id (primary)
-# pra -- pra_id (primary), subscriber_id (foreign - cap_list)
-# spec_ops -- spec_ops_id (primary), subscriber_id (foreign - cap_list)
-# utilization -- admission_id (primary), subscriber_id (foreign - cap_list)
-# client_track -- admission_id (foreign - utilization), spec_ops_id (foreign - spec_ops), pra_id (foreign - pra)
-
-
-# read in csv files
-
+  # cap_list -- subscriber_id (primary)
+  # pra -- pra_id (primary), subscriber_id (foreign - cap_list)
+  # spec_ops -- spec_ops_id (primary), subscriber_id (foreign - cap_list)
+  # utilization -- admission_id (primary), subscriber_id (foreign - cap_list)
+  # client_track -- admission_id (foreign - utilization), spec_ops_id (foreign - spec_ops), pra_id (foreign - pra)
 
 # Match: 
-# cap_list/pra
-# cap_list/spec_ops
-# cap_list/pra
-# client_track/pra
-# client_track/utilization
-# client_track/spec_ops
+  # cap_list/pra
+  # cap_list/spec_ops
+  # cap_list/utilization
+  # pra/client_track
+  # utilization/client_track
+  # spec_ops/client_track
 
-# Function to match keys
-  # Take two data frames (dfs are already read into R)
-  # Select primary key column from df1 and foreign key column from df2
-  # Match the two columns
-  # Select rows in df2 where foreign key does not match df1 primary key
-  # Flag these df2 rows. Remove them from df2?
+# Test data frames
+# df1 <- data.frame(df1_id = sample(1:15, 10), df1_x = c(1:10))
+# df2 <- data.frame(df2_x = c(1:10), df1_id = sample(1:15, 10))
 
+##################################
 
-function(df1, df2, df1primarykey, df2foreignkey){
+# Set working directories
+setwd('Y:/database')
+
+# Read in csv files
+cap_list <- read.csv("cap_list.csv")
+pra <- read.csv("pra.csv")
+spec_ops <- read.csv("spec_ops.csv")
+utilization <- read.csv("utilization.csv")
+client_track <- read.csv("client_track.csv")
+
+#matchr function
+matchr <- function(df1, df2, col){
   library(dplyr)
-  
-  x <- select(df1, df1primarykey)
-  y <- select(df2, df2foreignkey)
+  df2_match <<- semi_join(df2, df1, by = col)
+  df2_no_match <<- anti_join(df2, df1, by = col)
 }
